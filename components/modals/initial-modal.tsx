e483432f122e23153be55fa74db6731b3ab9a5b4
TadevosyannGarik <tadevosyan12@gmail.com>
2023-12-15 12:51:44 +0400

    UploadThing setup

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -3,10 +3,11 @@
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { FileUpload } from "@/components/file-upload";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useForm } from "react-hook-form";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import * as z from "zod";
 
 
@@ -59,29 +60,39 @@ export const InitialModal = () => {
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                         <div className="space-y-8 px-6">
                             <div className="flex items-center justify-center text-center">
-                                TODO: Image Upload
+                                <FormField control={form.control} name="imageUrl" render={({ field }) => (
+                                    <FormItem>
+                                        <FormControl>
+                                            <FileUpload 
+                                                endpoint="serverImage"
+                                                value={field.value}
+                                                onChange={field.onChange}
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}>
+                                    
+                                </FormField>
                             </div>
                             <FormField 
-                            control={form.control}
-                            name="name"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel className=" uppercase text-sm font-bold text-zinc-500 dark:text-secondary/70">
-                                        Server name
-                                    </FormLabel>
-                                    <FormControl>
-                                        <Input 
-                                            disabled={isLoading}
-                                            className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                                            placeholder="Enter server name"
-                                            {...field}
-                                        />
-                                    </FormControl>
-                                    <FormMessage 
-
-                                    />
-                                </FormItem>
-                            )}
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel className=" uppercase text-sm font-bold text-zinc-500 dark:text-secondary/70">
+                                            Server name
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input 
+                                                disabled={isLoading}
+                                                className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                                                placeholder="Enter server name"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
                             />
                         </div>
                         <DialogFooter className=" bg-gray-100 px-6 py-4">

7340378d8e6e2dea9daeb6f8a1cf9e8afa296c9a
TadevosyannGarik <tadevosyan12@gmail.com>
2023-12-21 22:18:48 +0400

    fix bags

--- app/api/channels/[channelId]/route.ts
+++ app/api/channels/[channelId]/route.ts
@@ -1,28 +1,27 @@
-import { currentProfile } from "@/lib/current-profile";
-import { NextResponse } from "next/server"
-import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
 import { MemberRole } from "@prisma/client";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
 
-
-export async function DELETE(req: Request, { params }: { params: { channelId: string }}) {
+export async function DELETE(req: Request, { params }: { params: { channelId: string } }) {
     try {
         const profile = await currentProfile();
         const { searchParams } = new URL(req.url);
-  
+
         const serverId = searchParams.get("serverId");
-  
+
         if (!profile) {
             return new NextResponse("Unauthorized", { status: 401 });
         }
-  
+
         if (!serverId) {
             return new NextResponse("Server ID missing", { status: 400 });
         }
-  
+
         if (!params.channelId) {
             return new NextResponse("Channel ID missing", { status: 400 });
         }
-  
+
         const server = await db.server.update({
             where: {
                 id: serverId,
@@ -41,12 +40,12 @@ export async function DELETE(req: Request, { params }: { params: { channelId: st
                         id: params.channelId,
                         name: {
                             not: "general",
-                        }
+                        }   
                     }
                 }
             }
         });
-  
+
         return NextResponse.json(server);
     } catch (error) {
         console.log("[CHANNEL_ID_DELETE]", error);
@@ -54,31 +53,30 @@ export async function DELETE(req: Request, { params }: { params: { channelId: st
     }
 }
 
-
-export async function PATCH(req: Request, { params }: { params: { channelId: string } }) {
+export async function PATCH(req: Request,{ params }: { params: { channelId: string } }) {
     try {
         const profile = await currentProfile();
         const { name, type } = await req.json();
         const { searchParams } = new URL(req.url);
-  
+
         const serverId = searchParams.get("serverId");
-  
+
         if (!profile) {
             return new NextResponse("Unauthorized", { status: 401 });
         }
-  
+
         if (!serverId) {
             return new NextResponse("Server ID missing", { status: 400 });
         }
-  
+
         if (!params.channelId) {
             return new NextResponse("Channel ID missing", { status: 400 });
         }
-  
+
         if (name === "general") {
             return new NextResponse("Name cannot be 'general'", { status: 400 });
         }
-  
+
         const server = await db.server.update({
             where: {
                 id: serverId,
@@ -92,26 +90,26 @@ export async function PATCH(req: Request, { params }: { params: { channelId: str
                 }
             },
             data: {
-            channels: {
-                update: {
-                    where: {
-                        id: params.channelId,
-                        NOT: {
-                            name: "general",
+                channels: {
+                    update: {
+                        where: {
+                            id: params.channelId,
+                            NOT: {
+                                name: "general",
+                            },
                         },
-                    },
-                    data: {
-                        name,
-                        type,
+                        data: {
+                            name,
+                            type,
+                        }
                     }
                 }
             }
-        }
-    });
-  
+        });
+
         return NextResponse.json(server);
     } catch (error) {
         console.log("[CHANNEL_ID_PATCH]", error);
         return new NextResponse("Internal Error", { status: 500 });
     }
-  }
+}

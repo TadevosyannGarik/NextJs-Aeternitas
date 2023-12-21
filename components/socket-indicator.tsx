dd84fd3b5cc2d1b4bf764d3491a0adcf2bd618d3
TadevosyannGarik <tadevosyan12@gmail.com>
2023-12-21 23:40:55 +0400

    remove socket indicator

--- components/socket-indicator.tsx
+++ components/socket-indicator.tsx
@@ -4,18 +4,6 @@ import { useSocket } from "@/components/providers/socket-provider";
 import { Badge } from "@/components/ui/badge";
 
 export const SocketIndicator = () => {
-    const { isConnected } = useSocket();
-
-    if (!isConnected) {
-        return (
-            <Badge 
-                variant="outline" 
-                className="bg-yellow-600 text-white border-none rounded-md text-sm"
-            >
-                Fallback: Polling every 1s
-            </Badge>
-        )
-    }
 
     return (
         <Badge 

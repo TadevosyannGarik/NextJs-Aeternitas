7f01384b77be657cc00cf16e111f129bb73d9d77
TadevosyannGarik <tadevosyan12@gmail.com>
2023-12-24 21:09:34 +0400

    Add Indicator

--- components/socket-indicator.tsx
+++ components/socket-indicator.tsx
@@ -4,11 +4,23 @@ import { useSocket } from "@/components/providers/socket-provider";
 import { Badge } from "@/components/ui/badge";
 
 export const SocketIndicator = () => {
+    const { isConnected } = useSocket();
+
+    if (!isConnected) {
+        return (
+            <Badge 
+                variant="outline" 
+                className="bg-yellow-600 text-white border-none text-sm rounded-sm"
+            >
+                Fallback: Polling every 1s
+            </Badge>
+        )
+    }
 
     return (
         <Badge 
             variant="outline" 
-            className="bg-emerald-600 text-white border-none rounded-md text-sm"
+            className="bg-emerald-600 text-white border-none text-sm rounded-sm"
         >
             Live: Real-time updates
         </Badge>
